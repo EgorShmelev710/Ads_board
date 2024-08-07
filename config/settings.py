ed56a55ab7c7e33a04dbb22fd2c1b1736999da7f
Egor Shmelev <shmelevyegor25@gmail.com>
2024-08-06 23:10:27 -0300

    changes to docker

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,10 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.env')
+if os.getenv('DOCKER_ENV') == 'True':
+    load_dotenv(BASE_DIR / '.env.docker')
+else:
+    load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
